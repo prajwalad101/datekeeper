7d5529f9f61117c0d86f95bd993ca05f58e68b46
Prajwalad101 <prajwalad101@gmail.com>
2023-06-10 08:24:21 +0545

    refactor: remove third party dependencies

--- server.go
+++ server.go
@@ -1,13 +1,5 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
-
+// TODO: create a route to authenticate user
 func main() {
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3000")
 }
